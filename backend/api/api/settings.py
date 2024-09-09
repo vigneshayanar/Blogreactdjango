ce1cb6f4e330b89e422656b23cdd4b768b8490c4
vigneshayanar <vigneshaynar@gmai.com>
2024-09-09 16:01:57 +0530

    deploy files

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://blogreact-ypll.onrender.com/",
+    "https://blogreact-ypll.onrender.com",
 ]
 
 ROOT_URLCONF = 'api.urls'
